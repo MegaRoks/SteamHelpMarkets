a6cac2eca29ae221efbb3f973ca1b067f40b746d
MegaRoks <MegaRoks@gmail.com>
2018-04-22 01:08:16 +0600

    Added buttons for csgo and pubg

--- SteamHelpMarkets.user.js
+++ SteamHelpMarkets.user.js
@@ -2,7 +2,7 @@
 // @name         SteamHelpMarkets
 // @description  Allows you to quickly see the subject on Dota2Wiki and Dota2Market
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.4
 // @author       MegaRoks
 // @match        *://steamcommunity.com/*/*/inventory/*
 // @icon        https://raw.githubusercontent.com/MegaRoks/SteamHelpMarkets/master/favicon.jpg
@@ -20,7 +20,17 @@
             if(game_name === 'Dota 2'){
                 var val = document.getElementById("iteminfo0_item_name").innerHTML;
                 var dfa = delete_for_array(val);
-                inventory_logos0(dfa);
+                inventory_dota0(dfa);
+            }
+            if(game_name === 'Counter-Strike: Global Offensive'){
+                var val = document.getElementById("iteminfo0_item_name").innerHTML;
+                var dfa = delete_for_array(val);
+                inventory_csgo0(dfa);
+            }
+            if(game_name === 'PLAYERUNKNOWN\'S BATTLEGROUNDS'){
+                var val = document.getElementById("iteminfo0_item_name").innerHTML;
+                var dfa = delete_for_array(val);
+                inventory_pubg0(dfa);
             }
         }
     );
@@ -34,7 +44,17 @@
             if(game_name === 'Dota 2'){
                 var val = document.getElementById("iteminfo1_item_name").innerHTML;
                 var dfa = delete_for_array(val);
-                inventory_logos1(dfa);
+                inventory_dota1(dfa);
+            }
+            if(game_name === 'Counter-Strike: Global Offensive'){
+                var val = document.getElementById("iteminfo1_item_name").innerHTML;
+                var dfa = delete_for_array(val);
+                inventory_csgo1(dfa);
+            }
+            if(game_name === 'PLAYERUNKNOWN\'S BATTLEGROUNDS'){
+                var val = document.getElementById("iteminfo1_item_name").innerHTML;
+                var dfa = delete_for_array(val);
+                inventory_pubg1(dfa);
             }
         }
     );
@@ -51,17 +71,21 @@ function delete_for_button_green(){
 
 function delete_for_array(val){
     var d2m = val.replace(/ /g, '%20');
+    var csm = d2m.replace(/\|/g, '%7C');
+    console.log(d2m);
     var d2o = val.replace(/ /g, '+');
+    var cso = d2o.replace(/\|/g, '%7C');
+    console.log(cso);
     var quality = ["Ascendant", "Auspicious", "Autographed", "Corrupted", "Elder", "Exalted", "Frozen", "Frozen", "Heroic", "Inscribed", "Inscribed", "Genuine"];
     var word = val.substr(0, val.indexOf(' '));
     if (quality.indexOf(word) !== -1){
         val = val.slice(word.length + 1);
     }
     var d2w = val.replace(/ /g, '_');
-    return [d2w, d2m, d2o];
+    return [d2w, d2m, d2o, csm, cso];
 }
 
-function inventory_logos0(dfa){
+function inventory_dota0(dfa){
     var d0 = document.getElementById('iteminfo0_item_tags');
     d0.insertAdjacentHTML('afterend', `
         <a class="item_market_action_button item_market_action_button_green button_green" href="https://dota2-ru.gamepedia.com/${dfa[0]}" target="_blank" id="quicksellbtn" style="margin-right: 12px; margin-bottom: 8px;">
@@ -85,7 +109,7 @@ function inventory_logos0(dfa){
     `);
 }
 
-function inventory_logos1(dfa){
+function inventory_dota1(dfa){
     var d1 = document.getElementById('iteminfo1_item_tags');
     d1.insertAdjacentHTML('afterend', `
         <a class="item_market_action_button item_market_action_button_green button_green" href="https://dota2-ru.gamepedia.com/${dfa[0]}" target="_blank" id="quicksellbtn" style="margin-right: 12px; margin-bottom: 8px;">
@@ -103,6 +127,66 @@ function inventory_logos1(dfa){
         <a class="item_market_action_button item_market_action_button_green button_green" href="https://ru.opskins.com/?loc=shop_search&app=570_2&search_item=${dfa[2]}&sort=lh" target="_blank" id="quicksellbtn" style="margin-right: 12px; margin-bottom: 8px;">
             <span class="item_market_action_button_edge item_market_action_button_left"></span>
             <span class="item_market_action_button_contents">Dota2Opskins</span>
+            <span class="item_market_action_button_edge item_market_action_button_right"></span>
+            <span class="item_market_action_button_preload"></span>
+        </a>
+    `);
+}
+
+function inventory_csgo0(dfa){
+    var d0 = document.getElementById('iteminfo0_item_tags');
+    d0.insertAdjacentHTML('afterend', `
+        <a class="item_market_action_button item_market_action_button_green button_green" href="https://market.csgo.com/?search=${dfa[3]}" target="_blank" id="quicksellbtn" style="margin-right: 12px; margin-bottom: 8px;">
+            <span class="item_market_action_button_edge item_market_action_button_left"></span>
+            <span class="item_market_action_button_contents">CSGOMarket</span>
+            <span class="item_market_action_button_edge item_market_action_button_right"></span>
+            <span class="item_market_action_button_preload"></span>
+        </a>
+    `);
+}
+
+function inventory_csgo1(dfa){
+    var d0 = document.getElementById('iteminfo1_item_tags');
+    d0.insertAdjacentHTML('afterend', `
+        <a class="item_market_action_button item_market_action_button_green button_green" href="https://market.csgo.com/?search=${dfa[3]}" target="_blank" id="quicksellbtn" style="margin-right: 12px; margin-bottom: 8px;">
+            <span class="item_market_action_button_edge item_market_action_button_left"></span>
+            <span class="item_market_action_button_contents">CSGOMarket</span>
+            <span class="item_market_action_button_edge item_market_action_button_right"></span>
+            <span class="item_market_action_button_preload"></span>
+        </a>
+    `);
+}
+
+function inventory_pubg0(dfa){
+    var d0 = document.getElementById('iteminfo0_item_tags');
+    d0.insertAdjacentHTML('afterend', `
+        <a class="item_market_action_button item_market_action_button_green button_green" href="https://pubg.tm/?search=${dfa[1]}" target="_blank" id="quicksellbtn" style="margin-right: 12px; margin-bottom: 8px;">
+            <span class="item_market_action_button_edge item_market_action_button_left"></span>
+            <span class="item_market_action_button_contents">PUBGMarket</span>
+            <span class="item_market_action_button_edge item_market_action_button_right"></span>
+            <span class="item_market_action_button_preload"></span>
+        </a>
+        <a class="item_market_action_button item_market_action_button_green button_green" href="https://ru.opskins.com/?loc=shop_search&app=578080_2&search_item=${dfa[2]}&sort=lh" target="_blank" id="quicksellbtn" style="margin-right: 12px; margin-bottom: 8px;">
+            <span class="item_market_action_button_edge item_market_action_button_left"></span>
+            <span class="item_market_action_button_contents">PUBGOpskins</span>
+            <span class="item_market_action_button_edge item_market_action_button_right"></span>
+            <span class="item_market_action_button_preload"></span>
+        </a>
+    `);
+}
+
+function inventory_pubg1(dfa){
+    var d1 = document.getElementById('iteminfo1_item_tags');
+    d1.insertAdjacentHTML('afterend', `
+        <a class="item_market_action_button item_market_action_button_green button_green" href="https://pubg.tm/?search=${dfa[1]}" target="_blank" id="quicksellbtn" style="margin-right: 12px; margin-bottom: 8px;">
+            <span class="item_market_action_button_edge item_market_action_button_left button_green"></span>
+            <span class="item_market_action_button_contents button_green">PUBGMarket</span>
+            <span class="item_market_action_button_edge item_market_action_button_right button_green"></span>
+            <span class="item_market_action_button_preload button_green"></span>
+        </a>
+        <a class="item_market_action_button item_market_action_button_green button_green" href="https://ru.opskins.com/?loc=shop_search&app=578080_2&search_item=${dfa[2]}&sort=lh" target="_blank" id="quicksellbtn" style="margin-right: 12px; margin-bottom: 8px;">
+            <span class="item_market_action_button_edge item_market_action_button_left"></span>
+            <span class="item_market_action_button_contents">PUBGOpskins</span>
             <span class="item_market_action_button_edge item_market_action_button_right"></span>
             <span class="item_market_action_button_preload"></span>
         </a>
