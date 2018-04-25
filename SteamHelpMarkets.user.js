6749e56e4b14604110c0633056819608940ea78a
MegaRoks <MegaRoks@gmail.com>
2018-04-25 20:41:48 +0600

    The extension no longer adds to the gems and button coupons

--- SteamHelpMarkets.user.js
+++ SteamHelpMarkets.user.js
@@ -2,7 +2,7 @@
 // @name         SteamHelpMarkets
 // @description  Allows you to quickly see the subject on Dota2Wiki and Dota2Market
 // @namespace    http://tampermonkey.net/
-// @version      0.7
+// @version      0.8
 // @author       MegaRoks
 // @match        *://steamcommunity.com/*/*/inventory/*
 // @icon        https://raw.githubusercontent.com/MegaRoks/SteamHelpMarkets/master/favicon.jpg
@@ -10,42 +10,51 @@
 // @grant        none
 // ==/UserScript==
 
+var id_games_for_market = ["730_2", "440_2", "570_2", "578080_2", "Подарок"];
+
 (function() {
-    console.log("Старт");
     document.getElementById("iteminfo0_item_name").addEventListener(
         "DOMSubtreeModified",
-        function() {
-            delete_for_button_green();
-            var games = names_gemes();
-            console.log(games);
-            var val = document.getElementById("iteminfo0_item_name").innerHTML;
-            var array_for_name = delete_for_array(val);
-            var array_for_href = games[0](array_for_name, games);
-            inventory_dota0(array_for_href, games);
-            console.log(1);
+        function(item_type) {
+            document.getElementById("iteminfo0_item_type").addEventListener("DOMSubtreeModified", function() {
+                delete_for_button_green();
+                var games = names_gemes();
+                var val = document.getElementById("iteminfo0_item_name").innerHTML;
+                var item_type = document.getElementById("iteminfo0_item_type").innerHTML;
+                if (val.indexOf("Самоцветы") >= 0 || val.indexOf("скидка") >= 0) {
+                }
+                else {
+                    var array_for_name = delete_for_array(val);
+                    var array_for_href = games[0](array_for_name, games);
+                    inventory_dota0(array_for_href, games, item_type);
+                }
+            });
         }
     );
 
     document.getElementById("iteminfo1_item_name").addEventListener(
         "DOMSubtreeModified",
-        function() {
-            delete_for_button_green();
-            var games = names_gemes();
-            console.log(games);
-            var val = document.getElementById("iteminfo1_item_name").innerHTML;
-            var array_for_name = delete_for_array(val);
-            var array_for_href = games[0](array_for_name, games);
-            inventory_dota1(array_for_href, games);
-            console.log(2);
+        function(item_type) {
+            document.getElementById("iteminfo1_item_type").addEventListener("DOMSubtreeModified", function() {
+                delete_for_button_green();
+                var games = names_gemes();
+                var val = document.getElementById("iteminfo1_item_name").innerHTML;
+                var item_type = document.getElementById("iteminfo1_item_type").innerHTML;
+                if (val.indexOf("Самоцветы") >= 0 || val.indexOf("скидка") >= 0) {
+                }
+                else {
+                    var array_for_name = delete_for_array(val);
+                    var array_for_href = games[0](array_for_name, games);
+                    inventory_dota1(array_for_href, games, item_type);
+                }
+            });
         }
     );
 })();
 
 function names_gemes(){
-    console.log("Определение вкладки");
     var active_tab = document.getElementsByClassName("games_list_tab active")[0];
     var game_name = active_tab.getElementsByClassName("games_list_tab_name")[0].innerHTML;
-    console.log(game_name);
     var games = {
         'Counter-Strike: Global Offensive': [add_for_href, "730_2", "https://market.csgo.com/?search="],
         'Team Fortress 2': [add_for_href, "440_2", "https://tf2.tm/?search="],
@@ -78,7 +87,6 @@ function names_gemes(){
 }
 
 function add_for_href(array_for_name, games){
-    console.log("Добавление в ссылку");
     var wiki_href = "https://dota2-ru.gamepedia.com/" + array_for_name[0];
     var market_href = games[2] + array_for_name[1];
     var opskins_href = "https://ru.opskins.com/?loc=shop_search&app=" + games[1] + "&search_item=" + array_for_name[2] + "&sort=lh";
@@ -86,7 +94,6 @@ function add_for_href(array_for_name, games){
 }
 
 function delete_for_button_green(){
-    console.log("Удаление кнопок");
     var elem = document.getElementsByClassName("button_green");
     if (elem.length > 0) {
         for(var i = 0; i < elem.length; i++){
@@ -96,36 +103,30 @@ function delete_for_button_green(){
 }
 
 function delete_for_array(val){
-    console.log("Удаление пробелов");
-    var market = val.replace(/ /g, '%20');
-    var csm = market.replace(/\|/g, '%7C');
-    var opskins = val.replace(/ /g, '+');
-    var cso = opskins.replace(/\|/g, '%7C');
+    var market = val.replace(/ /g, '%20').replace(/\|/g, '%7C');
+    var opskins = val.replace(/ /g, '+').replace(/:/g, '%3A').replace(/\|/g, '%7C');
     var quality = ["Ascendant", "Auspicious", "Autographed", "Corrupted", "Elder", "Exalted", "Frozen", "Frozen", "Heroic", "Inscribed", "Inscribed", "Genuine"];
     var word = val.substr(0, val.indexOf(' '));
     if (quality.indexOf(word) !== -1){
         val = val.slice(word.length + 1);
     }
     var wiki = val.replace(/ /g, '_');
-    return [wiki, market, opskins, csm, cso];
+    return [wiki, market, opskins];
 }
 
-function inventory_dota0(array_for_href, games){
-    console.log(3);
-    console.log(games);
-    var id_games_for_market = ["730_2", "440_2", "570_2", "578080_2", "753_6"];
+function inventory_dota0(array_for_href, games, item_type){
     var iteminfo0 = document.getElementById('iteminfo0_item_tags');
     if(games[1] === "570_2"){
         iteminfo0.insertAdjacentHTML('afterend', `
             <a class="item_market_action_button item_market_action_button_green button_green" href="${array_for_href[0]}" target="_blank" id="quicksellbtn" style="margin-right: 12px; margin-bottom: 8px;">
                 <span class="item_market_action_button_edge item_market_action_button_left"></span>
-            <span class="item_market_action_button_contents">Wiki</span>
+                <span class="item_market_action_button_contents">Wiki</span>
                 <span class="item_market_action_button_edge item_market_action_button_right"></span>
                 <span class="item_market_action_button_preload"></span>
             </a>`);
     }
     id_games_for_market.some(q => {
-        if (games[1].startsWith(q)) {
+        if (games[1].startsWith(q)|| item_type.indexOf(q) !== -1) {
             iteminfo0.insertAdjacentHTML('afterend', `
                 <a class="item_market_action_button item_market_action_button_green button_green" href="${array_for_href[1]}" target="_blank" id="quicksellbtn" style="margin-right: 12px; margin-bottom: 8px;">
                     <span class="item_market_action_button_edge item_market_action_button_left"></span>
@@ -146,22 +147,19 @@ function inventory_dota0(array_for_href, games){
     `);
 }
 
-function inventory_dota1(array_for_href, games){
-    console.log(4);
-    console.log(games);
-    var id_games_for_market = ["730_2", "440_2", "570_2", "578080_2", "753_6"];
+function inventory_dota1(array_for_href, games, item_type){
     var iteminfo1 = document.getElementById('iteminfo1_item_tags');
     if(games[1] === "570_2"){
         iteminfo1.insertAdjacentHTML('afterend', `
             <a class="item_market_action_button item_market_action_button_green button_green" href="${array_for_href[0]}" target="_blank" id="quicksellbtn" style="margin-right: 12px; margin-bottom: 8px;">
                 <span class="item_market_action_button_edge item_market_action_button_left"></span>
-            <span class="item_market_action_button_contents">Wiki</span>
+                <span class="item_market_action_button_contents">Wiki</span>
                 <span class="item_market_action_button_edge item_market_action_button_right"></span>
                 <span class="item_market_action_button_preload"></span>
             </a>`);
     }
     id_games_for_market.some(q => {
-        if (games[1].startsWith(q)) {
+        if (games[1].startsWith(q) || item_type.indexOf(q) !== -1) {
             iteminfo1.insertAdjacentHTML('afterend', `
                 <a class="item_market_action_button item_market_action_button_green button_green" href="${array_for_href[1]}" target="_blank" id="quicksellbtn" style="margin-right: 12px; margin-bottom: 8px;">
                     <span class="item_market_action_button_edge item_market_action_button_left"></span>
